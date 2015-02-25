cae3eb4e0196f67ffacf4d02a6822b3d12ae4395
diboy2 <cagaanan@usc.edu>
2015-02-24 18:39:08 -0800

    Added Xcode files

---
+++ Lab3/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Lab3
+//
+//  Created by Adrian on 2/24/15.
+//  Copyright (c) 2015 Adrian. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
