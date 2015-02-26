35d77b376023d232ed48bc5cf650564ed47915c4
diboy2 <cagaanan@usc.edu>
2015-02-25 20:19:26 -0800

    Finished Project

---
+++ Lab3/QuotesModel.h
@@ -0,0 +1,21 @@
+//
+//  QuotesModel.h
+//  Lab3
+//
+//  Created by Adrian on 2/24/15.
+//  Copyright (c) 2015 Adrian. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface QuotesModel : NSObject
+
+- (NSDictionary *) randomQuote;
+- (NSUInteger) numberOfQuotes;
+- (NSDictionary *) quoteAtIndex: (NSUInteger) index;
+- (void) removeQuoteAtIndex: (NSUInteger) index;
+- (void) insertQuote: (NSString *) quote author: (NSString *) author atIndex: (NSUInteger) index;
+- (void) insertQuote: (NSDictionary *) quote atIndex: (NSUInteger) index;
+- (NSDictionary *) nextQuote;
+- (NSDictionary *) prevQuote;
+@end
---
+++ Lab3/QuotesModel.m
@@ -0,0 +1,93 @@
+//
+//  QuotesModel.m
+//  Lab3
+//
+//  Created by Adrian on 2/24/15.
+//  Copyright (c) 2015 Adrian. All rights reserved.
+//
+
+#import "QuotesModel.h"
+// class extension
+@interface QuotesModel ()
+// private properties
+@property(strong, nonatomic)NSMutableArray *quotes;
+@property(strong, nonatomic)NSDictionary *currentQuote;
+@property NSUInteger currentIndex;
+
+@end
+
+@implementation QuotesModel
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        _quotes = [[NSMutableArray alloc] initWithObjects:
+                    [NSDictionary dictionaryWithObjectsAndKeys:@"Clint Eastwood",@"author",@"There's only one way to have a happy mariage and as soon as I learn what it is I'll get married again.",@"quote",nil],
+                    [NSDictionary dictionaryWithObjectsAndKeys:@"The cars", @"author",@"Life's the same, except for the shoes",@"quote", nil],
+                    [NSDictionary dictionaryWithObjectsAndKeys:@"Mark Twain",@"author",@"Go to Heaven for the climate, Hell for the company.",@"quote", nil],
+                    nil];
+        self.currentIndex = 0;
+    }
+    return self;
+}
+
+- (NSDictionary *) randomQuote{
+    NSUInteger index = random() % [self numberOfQuotes];
+    self.currentIndex = index;
+    return [self quoteAtIndex:index];
+}
+
+- (NSUInteger) numberOfQuotes{
+    return [self.quotes count];
+}
+
+- (NSDictionary *) quoteAtIndex: (NSUInteger) index{
+    return [self.quotes objectAtIndex:index];
+}
+
+- (void) removeQuoteAtIndex: (NSUInteger) index{
+    NSUInteger numOfQuotes = [self numberOfQuotes];
+    if(index < numOfQuotes){
+        [self.quotes removeObjectAtIndex:index];
+    }
+    
+}
+
+- (void) insertQuote: (NSString *) quote author: (NSString *) author atIndex: (NSUInteger) index{
+    NSUInteger numOfQuotes = [self numberOfQuotes];
+    if(index <= numOfQuotes){
+        [self.quotes
+         insertObject:[NSDictionary dictionaryWithObjectsAndKeys:author,@"author",quote,@"quote", nil]
+         atIndex:index];
+    }
+    
+}
+
+- (void) insertQuote: (NSDictionary *) quote atIndex: (NSUInteger) index{
+    NSUInteger numOfQuotes = [self numberOfQuotes];
+    if(index <= numOfQuotes){
+        [self.quotes
+         insertObject:quote
+         atIndex:index];
+    }
+}
+
+- (NSDictionary *) nextQuote{
+    self.currentIndex = (self.currentIndex + 1) % [self numberOfQuotes];
+    return [self.quotes objectAtIndex:self.currentIndex];
+}
+
+- (NSDictionary *) prevQuote{
+    
+    if(self.currentIndex == 0){
+        self.currentIndex = [self numberOfQuotes] -1;
+    }else{
+        self.currentIndex = (self.currentIndex - 1);
+    }
+    
+    return [self.quotes objectAtIndex:self.currentIndex];
+}
+
+
+
+@end
