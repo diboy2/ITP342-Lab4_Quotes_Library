4b9cb2df3bcc62f1e434227ca0ba42ece8cd19c9
diboy2 <cagaanan@usc.edu>
2015-03-20 14:06:27 -0700

    first commit

---
+++ Lab4/InputViewController.h
@@ -0,0 +1,13 @@
+//
+//  InputViewController.h
+//  Lab4
+//
+//  Created by Adrian on 3/20/15.
+//  Copyright (c) 2015 Adrian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InputViewController : UIViewController
+
+@end
